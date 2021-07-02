142304b2985445e4bce07fa621aa530dda2de704
pacino <pacino.kang@gmail.com>
2021-07-02 23:24:49 +0900

    first

--- src/main/java/me/kang/solr/SolrIndexingApp.java
+++ src/main/java/me/kang/solr/SolrIndexingApp.java
@@ -1,15 +1,15 @@
 package me.kang.solr;
 
-import org.springframework.boot.CommandLineRunner;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SolrIndexingApp {
     public static void main(String[] args) {
+
         SpringApplication.run(SolrIndexingApp.class, args);
+
     }
 }
-
-
